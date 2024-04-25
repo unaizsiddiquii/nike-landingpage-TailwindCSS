0cd06d35e410c57340ed96c9cbd8c7e451d2e159
Unaiz Siddiqui <unaiz4321@gmail.com>
2024-04-26 00:21:15 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProduct from "./PopularProduct";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  Footer,
+  PopularProduct,
+  Services,
+  Subscribe,
+  SpecialOffer,
+  SuperQuality,
+};
