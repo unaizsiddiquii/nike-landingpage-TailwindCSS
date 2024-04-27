b9868915f5d114231b4bf5630cc07e0dc5476d12
Unaiz Siddiqui <unaiz4321@gmail.com>
2024-04-27 21:53:47 +0530

    Added Nav bar

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,10 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section id="home"
+    className="w-full xl:flex-row flex-col justify-center min-h-screen gap-10 max-container border-2 border-red-400 p-2 ">
+
+    </section>
+  )
 };
 
 export default Hero;
