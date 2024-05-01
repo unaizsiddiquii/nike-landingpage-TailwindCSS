6154a4416884317afdfb93ca4532f196b8b838b5
Unaiz Siddiqui <unaiz4321@gmail.com>
2024-05-01 22:43:37 +0530

    Popular Product Section

--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,7 +1,25 @@
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
 const PopularProduct = () => {
   return (
-    <div>PopularProduct</div>
-  )
-}
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Popular </span>
+          Products
+        </h2>
+        <p className="lg:max-w-l g mt-2 font-montserrat text-slate-gray">
+          Expirience top-notch quality and style with our sought-after
+          selections, Discover a world of comfort, design, ans value
+        </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-colo-3 sm:grid-cols-2 grid-cols-1 sm:gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default PopularProduct
+export default PopularProduct;
